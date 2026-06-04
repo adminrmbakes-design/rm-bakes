ac678b2c597e43afd577058fea8b97ccc9a06091
adminrmbakes-design <admin.rmbakes@gmail.com>
2026-06-04 17:56:29 +0530

    Update admin_analytics_routes.py

--- routes/admin_analytics_routes.py
+++ routes/admin_analytics_routes.py
@@ -67,12 +67,7 @@ def analytics_dashboard():
     delivered_orders = Order.query.filter_by(order_status="delivered").count()
     cancelled_orders = Order.query.filter_by(is_cancelled=True).count()
 
-    # DESSERT STUDIO PIPELINE
-    pending_requests = CustomOrder.query.filter_by(custom_status="pending").count()
-    quoted_requests = CustomOrder.query.filter_by(custom_status="quoted").count()
-    approved_requests = CustomOrder.query.filter_by(custom_status="approved").count()
-    converted_requests = CustomOrder.query.filter_by(converted_to_order=True).count()
-
+    
     # PRODUCT INTELLIGENCE
     product_counter = Counter()
     product_revenue = Counter()
@@ -318,10 +313,6 @@ def analytics_dashboard():
         ready_orders=ready_orders,
         delivered_orders=delivered_orders,
         cancelled_orders=cancelled_orders,
-        pending_requests=pending_requests,
-        quoted_requests=quoted_requests,
-        approved_requests=approved_requests,
-        converted_requests=converted_requests,
         top_products=top_products,
         top_product_revenue=top_product_revenue,
         top_favourites=top_favourites,
