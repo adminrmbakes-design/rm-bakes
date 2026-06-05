a02ab003dc0cc6f4cbdfa874396f3e47bd3c78d6
adminrmbakes-design <admin.rmbakes@gmail.com>
2026-06-06 00:13:28 +0530

    Update review_routes.py

--- routes/review_routes.py
+++ routes/review_routes.py
@@ -4,6 +4,7 @@ from flask import redirect
 from flask import url_for
 from flask import request
 from flask import jsonify
+from flask import flash
 
 from flask_login import login_required
 from flask_login import current_user
