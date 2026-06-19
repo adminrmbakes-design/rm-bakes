76188ab26bdf831437af59bb901f2cae8f6ba37d
adminrmbakes-design <admin.rmbakes@gmail.com>
2026-06-19 11:40:29 +0530

    Update product_routes.py

--- routes/product_routes.py
+++ routes/product_routes.py
@@ -340,6 +340,16 @@ def product_details(product_id):
 
         )
 
+    featured_product_ids = [
+
+        slot.product_id
+
+        for slot in FeaturedProduct.query.filter(
+            FeaturedProduct.product_id.isnot(None)
+        ).all()
+
+    ]
+
   
 
     return render_template(
@@ -356,7 +366,9 @@ def product_details(product_id):
 
         written_reviews=written_reviews,
 
-        rating_only_reviews=rating_only_reviews
+        rating_only_reviews=rating_only_reviews,
+
+        featured_product_ids=featured_product_ids
 
     )
 
